37a1a2a1d5c2ac077931a218eaa9314fbf42dfd4
Adam Weinbaum <71104086+Adam746@users.noreply.github.com>
2025-08-29 11:47:48 -0400

    Update Main.java
    Implement time method to create aging in Main

--- Main.java
+++ Main.java
@@ -1,20 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
-        // Create a new Human instance
-        Human person = new Human("John Doe", 30, "Male", 6, "Brown");
+	public static void main(String[] args) {
+		// Create a new Human instance
+		Human person = new Human("John Doe", 30, "Male", 6, "Brown");
+		Time timer = new Time();
 
+		// Using the getter methods
+		System.out.println("Name: " + person.getName() + ", Age: " + person.getAge());
+		System.out.println("height: "+ person.getHeight() + ", hairColor: " + person.getHairColor());
 
+		// Using the setter methods
+		person.setName("Jane Doe");
+		
+		// Test the aging methods (wait 6 seconds)
+		person.setAge(28);
+		System.out.println("Person is " + person.getAge() + " years old.");
+		try {
+		    Thread.sleep(6000);
+		}
+		catch (Exception e) {
+		    System.out.println(e);
+		}
+		System.out.println("Person is " + person.getAge() + " years old.");
 
-        // Using the getter methods
-        System.out.println("Name: " + person.getName() + ", Age: " + person.getAge());
-        System.out.println("height: "+ person.getHeight() + ", hairColor: " + person.getHairColor());
-
-        // Using the setter methods
-        person.setName("Jane Doe");
-        person.setAge(28);
-        person.sleep(6);
-
-        // Invoking the custom method
-        person.talk("Hello, world!");
-    }
+		// Invoking the custom method
+		//person.talk("Hello, world!");
+	}
 }
